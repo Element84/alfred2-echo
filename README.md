b75cade6890896212f78a8aff654a5eb11527c53
Dan Pilone <dan@element84.com>
2013-10-16 22:52:50 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple NASA ECHO Collection Searcher
+
+A simple NASA ECHO Collection searcher that does keyword and free text searching against [NASA's ECHO Earth Data Clearing House] (https://api.echo.nasa.gov)
+
+## Usage
+
+Once installed, the workflow responds to 'echo'.  By itself, it returns all of the public collections in ECHO.  With an argument, it performs an ECHO keyword search (free text) against Collection metadata and shows the results.
+
---
+++ workflow/main.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# This is needed because Ruby 2.0 doesn't include '.' by default in the load path
+# and Mavericks ships with Ruby 2 now.
+($LOAD_PATH << File.expand_path("..", __FILE__)).uniq!
+
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require 'bundle/bundler/setup'
+require 'alfred'
+require 'net/http'
+require 'JSON'
+
+def url_from_query query
+  if query.empty?
+    'https://api.echo.nasa.gov/catalog-rest/echo_catalog/datasets.json'
+  else
+    "https://api.echo.nasa.gov/catalog-rest/echo_catalog/datasets.json?keyword=#{URI::encode(query)}"
+  end
+end
+
+def fetch_datasets url
+  uri = URI(url)
+  results = Net::HTTP.get(uri)
+  JSON.parse(results)
+end
+
+
+Alfred.with_friendly_error do |alfred|
+  datasets = fetch_datasets(url_from_query(ARGV.join(' ')))
+  alfred.ui.debug "Found #{datasets['feed']['entry'].count} datasets"
+
+  fb = alfred.feedback
+  datasets['feed']['entry'].each { |dataset|
+    alfred.ui.debug "Adding #{dataset['dataset_id']} with id: #{dataset['id']}..."
+    fb.add_item({
+      :uid      => dataset['id'],
+      :title    => dataset['dataset_id'],
+      :subtitle => dataset['links'][0]['href'],
+      :arg      => dataset['id'],
+      :valid    => 'yes',
+    })
+  }
+
+  puts fb.to_xml
+end
